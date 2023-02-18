5482b400f07e02ff82075b401c5521431a5ed7d1
Ononiwu Maureen <amaka013@gmail.com>
2023-02-18 10:53:14 +0100

    Attempting to fix shellcheck lint
    Signed-off-by: Ononiwu Maureen <amaka013@gmail.com>

--- .github/workflows/scripts/release-create-tracking-issue.sh
+++ .github/workflows/scripts/release-create-tracking-issue.sh
@@ -10,8 +10,8 @@ fi
 gh issue create -a "${GITHUB_ACTOR}" --repo "${REPO}" --label release --title "Release v${CANDIDATE_BETA}/v${CANDIDATE_STABLE}" --body "Like #4522, but for v${CANDIDATE_BETA}/v${CANDIDATE_STABLE}"
 **Performed by ocg release manager**
 
-- [ ] Prepare stable  release v${CANDIDATE_STABLE}
-- [ ] Prepare beta  release v${CANDIDATE_BETA}
-- [ ] Tag and release stable  v${CANDIDATE_STABLE}
-- [ ] Tag and release beta  v${CANDIDATE_BETA}
-- [ ] Prepare ocg-releases v${CANDIDATE_BETA}
+- [ ] Prepare stable  release v"${CANDIDATE_STABLE}"
+- [ ] Prepare beta  release v"${CANDIDATE_BETA}"
+- [ ] Tag and release stable  v"${CANDIDATE_STABLE}"
+- [ ] Tag and release beta  v"${CANDIDATE_BETA}"
+- [ ] Prepare ocg-releases v"${CANDIDATE_BETA}"
