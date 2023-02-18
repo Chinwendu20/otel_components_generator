4d1072f647b5be026be3015f1b7cecef6b2f8169
Maureen Ononiwu <amaka013@gmail.com>
2023-02-18 14:49:16 +0100

    Fixing shelllint errors
    Signed-off-by: Maureen Ononiwu <amaka013@gmail.com>

--- .github/workflows/scripts/release-create-tracking-issue.sh
+++ .github/workflows/scripts/release-create-tracking-issue.sh
@@ -7,10 +7,10 @@ if [ "${EXISTING_ISSUE}" != "" ]; then
     exit 0
 fi
 
-gh issue create -a "${GITHUB_ACTOR}" --repo "${REPO}" --label release --title "Release v${CANDIDATE_BETA}/v${CANDIDATE_STABLE}" --body "Release v${CANDIDATE_BETA}/v${CANDIDATE_STABLE}
+gh issue create -a "${GITHUB_ACTOR}" --repo "${REPO}" --label release --title "Release v${CANDIDATE_BETA}/v${CANDIDATE_STABLE}" --body "Like #4522, but for v${CANDIDATE_BETA}/v${CANDIDATE_STABLE}
 **Performed by collector release manager**
 
 - [ ] Prepare stable ocg release v${CANDIDATE_STABLE}
 - [ ] Prepare beta ocg release v${CANDIDATE_BETA}
 - [ ] Tag and release stable ocg v${CANDIDATE_STABLE}
-- [ ] Tag and release beta ocg v${CANDIDATE_BETA}
+- [ ] Tag and release beta ocg v${CANDIDATE_BETA}"
